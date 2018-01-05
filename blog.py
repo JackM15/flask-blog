9569fbc1c61eb55f8cc8b04258df3bc047615635
Jack <jackm550@googlemail.com>
2018-01-05 09:34:02 +0000

    Added main page and login page basic templates, added routes to blog.py

--- blog.py
+++ blog.py
@@ -17,5 +17,15 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+# routes
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+#run the app if launched from this file
 if __name__ == '__main__':
     app.run(debug=True)
